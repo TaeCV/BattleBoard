760a0725e27915235a0c8fa4ddb00463222f5664
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-04-23 20:13:13 +0700

    Update finished MenuBar

--- src/screen/MenuBar.java
+++ src/screen/MenuBar.java
@@ -10,26 +10,26 @@ public class MenuBar extends VBox {
 	public Button startButton;
 	public Button howToPlayButton;
 	public Button quitButton;
-	 
+
 	public MenuBar() {
 		setAlignment(Pos.BOTTOM_CENTER);
 		setPrefHeight(300);
 		setPrefWidth(180);
 		setSpacing(30);
-		setPadding(new Insets(200,50,200,50));
+		setPadding(new Insets(200, 50, 200, 50));
 		startButton = new ActionButton("START GAME");
 		howToPlayButton = new ActionButton("HOW TO PLAY");
 		quitButton = new ActionButton("QUIT");
-		
+
 		getChildren().addAll(startButton, howToPlayButton, quitButton);
 	}
-	
+
 	public void setButtonDisable() {
 		startButton.setDisable(true);
 		howToPlayButton.setDisable(true);
 		quitButton.setDisable(true);
 	}
-	
+
 	public void setButtonUsable() {
 		startButton.setDisable(false);
 		howToPlayButton.setDisable(false);
