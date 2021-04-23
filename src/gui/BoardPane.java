3e8cf033ac1d8ebc0cac6d868ec40702a42c2f13
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-04-23 20:13:51 +0700

    Update finished BoardPane

--- src/gui/BoardPane.java
+++ src/gui/BoardPane.java
@@ -15,32 +15,14 @@ public class BoardPane extends Canvas {
 		setWidth(700);
 		setHeight(600);
 		gc = getGraphicsContext2D();
-		for (int i = 0; i < getWidth(); i += PIXEL) {
-			if (i / PIXEL != 3) {
-				gc.drawImage(RenderableHolder.brick_wall_Image, i, 0, PIXEL, PIXEL);
-				gc.setFill(Color.CRIMSON);
-				gc.beginPath();
-				gc.moveTo(20 + i, 0);
-				gc.lineTo(20 + i, 30);
-				gc.lineTo(50 + i, 45);
-				gc.lineTo(80 + i, 30);
-				gc.lineTo(80 + i, 0);
-				gc.closePath();
-				gc.fill();
-				gc.stroke();
-			} else {
-				gc.drawImage(RenderableHolder.waterfall_Image, i, 0, PIXEL, PIXEL);
-			}
 
-		}
+		gc.drawImage(RenderableHolder.wall_Image, 0, 0, getWidth(), 50);
+		gc.drawImage(RenderableHolder.waterfall_Image, 300, 0, PIXEL, 50);
+
 		for (int i = 0; i < getHeight() - PIXEL; i += PIXEL) {
 			for (int j = 0; j < getWidth(); j += PIXEL) {
 				// Draw a floor
 				if (j / PIXEL != 3) {
-//					gc.setFill(Color.LIGHTGOLDENRODYELLOW);
-//					gc.setStroke(Color.WHITE);
-//					gc.fillRect(originY + j, originX + i, PIXEL, PIXEL);
-//					gc.strokeRect(originY + j, originX + i, PIXEL, PIXEL);
 					gc.drawImage(RenderableHolder.floor_Image, originY + j, originX + i, PIXEL, PIXEL);
 				} else {
 					gc.drawImage(RenderableHolder.river_Image, originY + j, originX + i, PIXEL, PIXEL);
@@ -53,14 +35,8 @@ public class BoardPane extends Canvas {
 
 			}
 		}
-		for (int i = 0; i < getWidth(); i += PIXEL) {
-			if (i / PIXEL != 3) {
-				gc.drawImage(RenderableHolder.stone_wall_Image, i, getHeight() - 50, PIXEL, PIXEL);
-			} else {
-				gc.drawImage(RenderableHolder.waterfall_Image, i - 5, getHeight() - 50, PIXEL + 10, PIXEL);
-			}
-
-		}
+		gc.drawImage(RenderableHolder.stone_wall_Image, 0, getHeight() - 50, getWidth(), PIXEL);
+		gc.drawImage(RenderableHolder.waterfall_Image,  295, getHeight() - 50, PIXEL + 10, PIXEL);
 	}
 
 }
