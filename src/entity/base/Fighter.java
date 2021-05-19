9cd408d8ec2e582012a842a151ef4f8c653d8270
Phitchaya Charansathianchai <gustphitchaya@gmail.com>
2021-05-19 13:00:51 +0700

    add gametie sound, deleted unused audios

--- src/entity/base/Fighter.java
+++ src/entity/base/Fighter.java
@@ -33,7 +33,7 @@ public abstract class Fighter implements Updatable {
 		setCoordinate(null);
 		setBaseStats();
 		setSpecialAbility();
-		setZ(-1);
+		setZ(-100);
 		attack = 150; // just for testing, delete this line
 	}
 
