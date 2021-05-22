a54a8f258dc60acd13d1263d73e7ef61c5b5ef1d
Phitchaya Charansathianchai <gustphitchaya@gmail.com>
2021-05-22 14:43:33 +0700

    rewrite some how to play

--- src/screen/HowToPlay.java
+++ src/screen/HowToPlay.java
@@ -36,10 +36,11 @@ public class HowToPlay extends VBox {
 		description.setVgap(10);
 		description.setPadding(new Insets(15));
 		int row_idx = 0;
-		Text howToPlay_text = new Text("Rules :\n 1) Pick 5 fighters from 8 random fighters, and play best of 3 round.\n"
-				+ "2) Each round, player will play around 7 turns.\n"
-				+ "3) Each turn, pick each fighter to do an action ( move, attack, or heal (healer only) ).\n"
-				+ "4) The round will end if time is up, or after playing 14 turns, or fighters are all dead.\n");
+		Text howToPlay_text = new Text("Rules :\n1) The game is best of 3 rounds.\n"
+				+ "2) Each round, both players pick 5 fighters from 8 random fighters.\n"
+				+ "3) Each round, player can play up to 9 turns.\n"
+				+ "4) Each turn, a player has 60 seconds to pick each fighter to do an action ( move, attack, or heal (healer only) ).\n"
+				+ "5) The round will end when\n 5.1) after each players played 9 turns\n 5.2) one player is out of fighters.\n");
 		howToPlay_text.setWrappingWidth(400);
 		howToPlay_text.setFont(Font.font("Palatino Linotype", FontWeight.SEMI_BOLD, 25));
 		description.add(howToPlay_text, 0, row_idx++, 2, 1);
@@ -55,7 +56,7 @@ public class HowToPlay extends VBox {
 		description.addRow(row_idx++, board_img, gameboard_description);
 
 		// Describe each fighter's ability
-		Text player_topic = new Text("Player description:");
+		Text player_topic = new Text("Fighters description:");
 		player_topic.setFont(Font.font("Palatino Linotype", FontWeight.SEMI_BOLD, 30));
 		description.add(player_topic, 0, row_idx++, 2, 1);
 		ImageView duck_img = new ImageView(RenderableHolder.duckmelee1_Image);
